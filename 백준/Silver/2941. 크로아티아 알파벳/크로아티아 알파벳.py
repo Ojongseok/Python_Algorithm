63b7b75e461c940ed2ab1788e9d33185ab880a23
오종석 <98886487+Ojongseok@users.noreply.github.com>
2023-10-03 15:08:35 +0900

    Update 크로아티아 알파벳.py

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,30 +1,9 @@
 str = input()
 answer = 0
 
-if "dz=" in str :
-    answer += str.count("dz=")
-    str = str.replace("dz=", ".")
-if "c-" in str :
-    answer += str.count("c-")
-    str = str.replace("c-", ".")
-if "c=" in str :
-    answer += str.count("c=")
-    str = str.replace("c=", ".")
-if "d-" in str :
-    answer += str.count("d-")
-    str = str.replace("d-", ".")
-if "lj" in str :
-    answer += str.count("lj")
-    str = str.replace("lj", ".")
-if "nj" in str :
-    answer += str.count("nj")
-    str = str.replace("nj", ".")
-if "s=" in str :
-    answer += str.count("s=")
-    str = str.replace("s=", ".")
-if "z=" in str :
-    answer += str.count("z=")
-    str = str.replace("z=", ".")
+croatia = ["c=", "c-","dz=","d-","lj","nj","s=","z="]
 
-str = str.replace(".","")
-print(answer + len(str))
+for i in croatia :
+    str = str.replace(i, ".")
+
+print(len(str))
