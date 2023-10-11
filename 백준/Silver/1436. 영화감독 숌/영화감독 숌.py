2285e719b00da1ba58ad8f1ee004bff506dce753
오종석 <98886487+Ojongseok@users.noreply.github.com>
2023-10-11 22:51:59 +0900

    [Silver V] Title: 영화감독 숌, Time: 924 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+movieNum = 665
+cnt = 0
+
+while cnt != n :
+    movieNum += 1
+    movieNumToStr = str(movieNum)
+    if ("666" in movieNumToStr) :
+        cnt+=1
+
+print(movieNum)
+    
