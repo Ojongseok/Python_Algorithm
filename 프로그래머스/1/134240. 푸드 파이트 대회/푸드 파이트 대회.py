ace2f27b63042b6aac6149f81ccd735db81d9a5d
오종석 <98886487+Ojongseok@users.noreply.github.com>
2024-01-18 21:32:39 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,9 @@
+def solution(food):
+    answer = "0"
+    
+    for i in range(len(food)-1,0,-1) :
+        for j in range(food[i]//2):
+            answer=str(i)+answer
+            answer+=str(i)
+    
+    return answer
